3f36eadea2ed657f3e0e033f72c59f071de38551
Adamrosdi88 <adamulah88@gmail.com>
2026-06-21 19:03:38 +0800

    Update dashboard design

--- WinFormsApp1/WinFormsApp1/DoctorDashboard.cs
+++ WinFormsApp1/WinFormsApp1/DoctorDashboard.cs
@@ -12,219 +12,224 @@ using WinFormsApp1.models;
 namespace WinFormsApp1
 {
 
-    public partial class DoctorDashboard : Form
-    {
-        private String _doctorId;
-        ClinicManagementSystemContext db = new ClinicManagementSystemContext();
-        public DoctorDashboard(string Email)
-        {
-            InitializeComponent();
-            var GetDoctorName = db.Doctors.FirstOrDefault(x => x.Email == Email);
-            if (GetDoctorName != null)
-            {
-                _doctorId = GetDoctorName.DoctorId;
-                ShowDoctorlbl.Text = $"Welcome Back {GetDoctorName.FullName}";
-            }
-            LoadTodayAppointments();
+	public partial class DoctorDashboard : Form
+	{
+		private String _doctorId;
+		ClinicManagementSystemContext db = new ClinicManagementSystemContext();
+		public DoctorDashboard(string Email)
+		{
+			InitializeComponent();
+			var GetDoctorName = db.Doctors.FirstOrDefault(x => x.Email == Email);
+			if (GetDoctorName != null)
+			{
+				_doctorId = GetDoctorName.DoctorId;
+				ShowDoctorlbl.Text = $"Welcome Back {GetDoctorName.FullName}";
+			}
+			LoadTodayAppointments();
 
-        }
+		}
 
-        private void LoadTodayAppointments()
-        {
-            var today = DateOnly.FromDateTime(DateTime.Today);
+		private void LoadTodayAppointments()
+		{
+			var today = DateOnly.FromDateTime(DateTime.Today);
 
-            var appointments = db.Appointments
-                .Where(x => x.DoctorId == _doctorId
-                    && x.AppointmentDate == today
-                    && (x.Status == "Approved" || x.Status == "Pending"))
-                .Select(x => new
-                {
-                    Id = x.AppointmentId,
-                    Patient = x.Patient.FullName,
-                    Time = x.AppointmentTime,
-                    Status = x.Status
-                })
-                .ToList();
+			var appointments = db.Appointments
+				.Where(x => x.DoctorId == _doctorId
+					&& x.AppointmentDate == today
+					&& (x.Status == "Approved" || x.Status == "Pending"))
+				.Select(x => new
+				{
+					Id = x.AppointmentId,
+					Patient = x.Patient.FullName,
+					Time = x.AppointmentTime,
+					Status = x.Status
+				})
+				.ToList();
 
-            TodayPatientDGV.DataSource = appointments;
+			TodayPatientDGV.DataSource = appointments;
 
-            TodayPatientDGV.Columns["Id"].Visible = false;
+			TodayPatientDGV.Columns["Id"].Visible = false;
 
-            if (!TodayPatientDGV.Columns.Contains("CompleteButton"))
-            {
-                DataGridViewButtonColumn completeBtn =
-                    new DataGridViewButtonColumn();
+			if (!TodayPatientDGV.Columns.Contains("CompleteButton"))
+			{
+				DataGridViewButtonColumn completeBtn =
+					new DataGridViewButtonColumn();
 
-                completeBtn.Name = "CompleteButton";
-                completeBtn.HeaderText = "Complete";
-                completeBtn.Text = "Complete";
-                completeBtn.UseColumnTextForButtonValue = true;
+				completeBtn.Name = "CompleteButton";
+				completeBtn.HeaderText = "Complete";
+				completeBtn.Text = "Complete";
+				completeBtn.UseColumnTextForButtonValue = true;
 
-                TodayPatientDGV.Columns.Add(completeBtn);
-            }
-            if (!TodayPatientDGV.Columns.Contains("CancelButton"))
-            {
-                DataGridViewButtonColumn cancelBtn =
-                    new DataGridViewButtonColumn();
+				TodayPatientDGV.Columns.Add(completeBtn);
+			}
+			if (!TodayPatientDGV.Columns.Contains("CancelButton"))
+			{
+				DataGridViewButtonColumn cancelBtn =
+					new DataGridViewButtonColumn();
 
-                cancelBtn.Name = "CancelButton";
-                cancelBtn.HeaderText = "Cancel";
-                cancelBtn.Text = "Cancel";
-                cancelBtn.UseColumnTextForButtonValue = true;
+				cancelBtn.Name = "CancelButton";
+				cancelBtn.HeaderText = "Cancel";
+				cancelBtn.Text = "Cancel";
+				cancelBtn.UseColumnTextForButtonValue = true;
 
-                TodayPatientDGV.Columns.Add(cancelBtn);
-            }
+				TodayPatientDGV.Columns.Add(cancelBtn);
+			}
 
-            TodayPatientDGV.AutoSizeColumnsMode =
-                   DataGridViewAutoSizeColumnsMode.Fill;
-        }
+			TodayPatientDGV.AutoSizeColumnsMode =
+				   DataGridViewAutoSizeColumnsMode.Fill;
+		}
 
-        private void TodayPatientDGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            if (e.RowIndex < 0)
-                return;
+		private void TodayPatientDGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+			if (e.RowIndex < 0)
+				return;
 
-            string appointmentId =
-                TodayPatientDGV.Rows[e.RowIndex]
-                .Cells["Id"]
-                .Value
-                .ToString();
+			string appointmentId =
+				TodayPatientDGV.Rows[e.RowIndex]
+				.Cells["Id"]
+				.Value
+				.ToString();
 
-            var appointment = db.Appointments
-                .FirstOrDefault(x => x.AppointmentId == appointmentId);
+			var appointment = db.Appointments
+				.FirstOrDefault(x => x.AppointmentId == appointmentId);
 
-            if (appointment == null)
-                return;
+			if (appointment == null)
+				return;
 
-            if (TodayPatientDGV.Columns[e.ColumnIndex].Name == "CompleteButton")
-            {
-                appointment.Status = "Completed";
+			if (TodayPatientDGV.Columns[e.ColumnIndex].Name == "CompleteButton")
+			{
+				appointment.Status = "Completed";
 
-                db.SaveChanges();
+				db.SaveChanges();
 
-                MessageBox.Show("Appointment completed.");
-            }
+				MessageBox.Show("Appointment completed.");
+			}
 
-            if (TodayPatientDGV.Columns[e.ColumnIndex].Name == "CancelButton")
-            {
-                appointment.Status = "Requested";
+			if (TodayPatientDGV.Columns[e.ColumnIndex].Name == "CancelButton")
+			{
+				appointment.Status = "Requested";
 
-                db.SaveChanges();
+				db.SaveChanges();
 
-                MessageBox.Show("Appointment returned to requested.");
-            }
+				MessageBox.Show("Appointment returned to requested.");
+			}
 
-            LoadTodayAppointments();
-            LoadRequestedAppointments();
+			LoadTodayAppointments();
+			LoadRequestedAppointments();
 
-        }
+		}
 
-        private void LoadRequestedAppointments()
-        {
-            var requests = db.Appointments
-        .Where(x => x.DoctorId == _doctorId
-            && x.Status == "Requested")
-        .Select(x => new
-        {
-            Id = x.AppointmentId,
-            Patient = x.Patient.FullName,
-            Date = x.AppointmentDate,
-            Time = x.AppointmentTime
-        })
-        .ToList();
+		private void LoadRequestedAppointments()
+		{
+			var requests = db.Appointments
+		.Where(x => x.DoctorId == _doctorId
+			&& x.Status == "Requested")
+		.Select(x => new
+		{
+			Id = x.AppointmentId,
+			Patient = x.Patient.FullName,
+			Date = x.AppointmentDate,
+			Time = x.AppointmentTime
+		})
+		.ToList();
 
-            CancellationRequestDGV.DataSource = requests;
-        }
+			CancellationRequestDGV.DataSource = requests;
+		}
 
-        private void CancellationRequestDGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
+		private void CancellationRequestDGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
 
-        }
-        private void LoadUpcomingAppointments()
-        {
-            var today = DateOnly.FromDateTime(DateTime.Today);
-            var weekEnd = today.AddDays(7); // inclusive end of the week window
+		}
+		private void LoadUpcomingAppointments()
+		{
+			var today = DateOnly.FromDateTime(DateTime.Today);
+			var weekEnd = today.AddDays(7); // inclusive end of the week window
 
-            var upcoming = db.Appointments
-                .Where(x => x.DoctorId == _doctorId
-                    && x.AppointmentDate > today
-                    && x.AppointmentDate <= weekEnd
-                    && (x.Status == "Approved" || x.Status == "Pending") )
-                .Select(x => new
-                {
-                    Id = x.AppointmentId,
-                    Patient = x.Patient.FullName,
-                    Date = x.AppointmentDate,
-                    Time = x.AppointmentTime,
-                    Status = x.Status
-                })
-                .ToList();
+			var upcoming = db.Appointments
+				.Where(x => x.DoctorId == _doctorId
+					&& x.AppointmentDate > today
+					&& x.AppointmentDate <= weekEnd
+					&& (x.Status == "Approved" || x.Status == "Pending"))
+				.Select(x => new
+				{
+					Id = x.AppointmentId,
+					Patient = x.Patient.FullName,
+					Date = x.AppointmentDate,
+					Time = x.AppointmentTime,
+					Status = x.Status
+				})
+				.ToList();
 
-            ThisWeekPatientDGV.DataSource = upcoming;
+			ThisWeekPatientDGV.DataSource = upcoming;
 
-            if (ThisWeekPatientDGV.Columns.Contains("Id"))
-                ThisWeekPatientDGV.Columns["Id"].Visible = false;
+			if (ThisWeekPatientDGV.Columns.Contains("Id"))
+				ThisWeekPatientDGV.Columns["Id"].Visible = false;
 
-            ThisWeekPatientDGV.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+			ThisWeekPatientDGV.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
 
-            if (!ThisWeekPatientDGV.Columns.Contains("CancelButton") && upcoming.Any())
-            {
-                var cancelBtn = new DataGridViewButtonColumn
-                {
-                    Name = "CancelButton",
-                    HeaderText = "Action",
-                    Text = "Cancel",
-                    UseColumnTextForButtonValue = true
-                };
-                ThisWeekPatientDGV.Columns.Add(cancelBtn);
-            }
-        }
+			if (!ThisWeekPatientDGV.Columns.Contains("CancelButton") && upcoming.Any())
+			{
+				var cancelBtn = new DataGridViewButtonColumn
+				{
+					Name = "CancelButton",
+					HeaderText = "Action",
+					Text = "Cancel",
+					UseColumnTextForButtonValue = true
+				};
+				ThisWeekPatientDGV.Columns.Add(cancelBtn);
+			}
+		}
 
-        private void ThisWeekPatientDGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            if (e.RowIndex < 0)
-                return;
+		private void ThisWeekPatientDGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+			if (e.RowIndex < 0)
+				return;
 
-            if (ThisWeekPatientDGV.Columns[e.ColumnIndex].Name == "CancelButton")
-            {
-                string appointmentId =
-                   ThisWeekPatientDGV.Rows[e.RowIndex]
-                    .Cells["Id"]
-                    .Value
-                    .ToString();
+			if (ThisWeekPatientDGV.Columns[e.ColumnIndex].Name == "CancelButton")
+			{
+				string appointmentId =
+				   ThisWeekPatientDGV.Rows[e.RowIndex]
+					.Cells["Id"]
+					.Value
+					.ToString();
 
-                var appointment = db.Appointments
-                    .FirstOrDefault(x => x.AppointmentId == appointmentId);
+				var appointment = db.Appointments
+					.FirstOrDefault(x => x.AppointmentId == appointmentId);
 
-                if (appointment != null)
-                {
-                    DialogResult result = MessageBox.Show(
-                        "Return this appointment to Requested status?",
-                        "Confirm",
-                        MessageBoxButtons.YesNo);
+				if (appointment != null)
+				{
+					DialogResult result = MessageBox.Show(
+						"Return this appointment to Requested status?",
+						"Confirm",
+						MessageBoxButtons.YesNo);
 
-                    if (result == DialogResult.Yes)
-                    {
-                        appointment.Status = "Requested";
+					if (result == DialogResult.Yes)
+					{
+						appointment.Status = "Requested";
 
-                        db.SaveChanges();
+						db.SaveChanges();
 
-                        MessageBox.Show("Appointment cancelled.");
+						MessageBox.Show("Appointment cancelled.");
 
-                        LoadUpcomingAppointments();
-                        LoadRequestedAppointments();
-                        LoadTodayAppointments();
-                    }
+						LoadUpcomingAppointments();
+						LoadRequestedAppointments();
+						LoadTodayAppointments();
+					}
 
-                }
-            }
-        }
+				}
+			}
+		}
 
-        private void Refreshbtn_Click(object sender, EventArgs e)
-        {
-            LoadTodayAppointments ();
-            LoadRequestedAppointments ();
-            LoadUpcomingAppointments (); 
-        }
-    }
+		private void Refreshbtn_Click(object sender, EventArgs e)
+		{
+			LoadTodayAppointments();
+			LoadRequestedAppointments();
+			LoadUpcomingAppointments();
+		}
+
+		private void DoctorDashboard_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
 }
